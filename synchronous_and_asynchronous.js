081031061a890ab1dbefa1c6a7942cd66b2c8aca
Deepraj-devikar <deeprajdevikar19@gmail.com>
2023-01-03 21:42:00 +0530

    ADDED: example of synchrounous and asynchronous

---
+++ synchronous_and_asynchronous.js
@@ -0,0 +1,23 @@
+//--------- synchronous----------
+// console.log("Hi");
+// console.log("Hello");
+// console.log("How are you");
+/**
+--------OUTPUT--------
+Hi
+Hello
+How are you
+ */
+
+//--------- asynchronous-----------------
+// console.log("Hi");
+// setTimeout(() => {
+//     console.log("Hello");
+// }, 2000);
+// console.log("How are you");
+/**
+--------OUTPUT-----------
+Hi
+How are you
+Hello
+ */
