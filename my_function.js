714725d12e311ccf826728591c28bf8ccbbc4993
Deepraj-devikar <deeprajdevikar19@gmail.com>
2023-01-03 17:44:57 +0530

    ADDED: normal function

---
+++ my_function.js
@@ -0,0 +1,8 @@
+// Function is block of code.
+
+// function declaration
+function isEven(num) {
+  return num % 2 === 0;
+}
+console.log(isEven(24)); // => true
+console.log(isEven(11)); // => false
