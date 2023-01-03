a4b3c0657804b64932559d5f7cd6801fd0a3e4bf
Deepraj-devikar <deeprajdevikar19@gmail.com>
2023-01-03 18:19:45 +0530

    ADDED: regular function

--- my_function.js
+++ my_function.js
@@ -11,3 +11,13 @@ console.log(isEven(24)); // => true
 console.log(isEven(11)); // => false
 console.log(isEven.name); // isEven
 console.log(typeof isEven); // function 
+
+// regular function
+function sum(value1, value2) {
+  return value1 + value2;
+}
+console.log(sum(5, 6));           // => 11
+console.log(([3, 7, 8, 6, 12]).reduce(sum)); // => 36
+console.log(([3, 7, 8, 6, 12]).reduce(sum, 11)); // => 47
+// array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+// currentIndex and arr is optional
