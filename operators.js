fc7ca521761164abdcf49317466de8f26077531b
Deepraj-devikar <deeprajdevikar19@gmail.com>
2023-01-06 15:08:12 +0530

    ADDED: oprator explore

---
+++ operators.js
@@ -0,0 +1,28 @@
+/**
+There are different types of JavaScript operators:
+
+Arithmetic Operators
+Assignment Operators
+Comparison Operators
+Logical Operators
+Conditional Operators
+Type Operators
+ */
+
+let a = 3;
+let x = (100 + 50) * a;
+
+console.log(a); // 3
+console.log(x); // 450
+
+/**
+Operator	Description
++	Addition
+-	Subtraction
+*	Multiplication
+**	Exponentiation (ES2016)
+/	Division
+%	Modulus (Division Remainder)
+++	Increment
+--	Decrement
+ */
